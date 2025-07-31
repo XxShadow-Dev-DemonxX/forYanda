db6704b205b6b0c3a5c9c7b4f91e419e5397b7bb
XxShadow-Dev-DemonxX <hafidzafsandi2405@gmail.com>
2025-07-31 14:43:04 +1200

    Integrating simple tkinter GUI

--- main.py
+++ main.py
@@ -5,31 +5,25 @@ import pandas as pd
 import glob
 from datetime import datetime
 
-# Load all CSV files
-files = glob.glob("data/*.csv")
-if not files:
-    raise FileNotFoundError("No CSV files found in data/ folder")
+def file_processing(files):
+    # Read metadata from the first file to determine filename
+    first_metadata, _ = read_file.parse_bank_csv(files[0])
+    from_dt = datetime.strptime(first_metadata["from_date"], "%Y%m%d")
+    to_dt = datetime.strptime(first_metadata["to_date"], "%Y%m%d")
+    from_date = from_dt.strftime('%d %b %Y')
+    to_date = to_dt.strftime('%d %b %Y')
 
-# Read metadata from the first file to determine filename
-first_metadata, _ = read_file.parse_bank_csv(files[0])
-from_dt = datetime.strptime(first_metadata["from_date"], "%Y%m%d")
-to_dt = datetime.strptime(first_metadata["to_date"], "%Y%m%d")
-from_date = from_dt.strftime('%d %b %Y')
-to_date = to_dt.strftime('%d %b %Y')
+    excel_filename = f"{from_date} to {to_date} Financial Statement.xlsx"
 
-excel_filename = f"{from_date} to {to_date} IIC Financial Statement.xlsx"
+    # Create Excel file with multiple sheets
+    with pd.ExcelWriter(excel_filename, engine="openpyxl") as writer:
+        for file in files:
+            metadata, table = read_file.parse_bank_csv(file)
+            temp = metadata["bank_info"].split(';')[2]              
+            temp = temp.split()
+            sheet = ' '.join(temp[1:])
+            final_table = format_table.format(metadata, table)
+            final_table.to_excel(writer, sheet_name=sheet, index=False)
 
-# Create Excel file with multiple sheets
-with pd.ExcelWriter(excel_filename, engine="openpyxl") as writer:
-    for file in files:
-        metadata, table = read_file.parse_bank_csv(file)
-        temp = metadata["bank_info"].split(';')[2]
-        temp = temp.split()
-        sheet = ' '.join(temp[1:])
-        final_table = format_table.format(metadata, table)
-        final_table.to_excel(writer, sheet_name=sheet, index=False)
-
-# Apply formatting to all sheets in the Excel file
-format_excel.format(excel_filename)
-
-print(f"All CSV files successfully written to {excel_filename}")
+    # Apply formatting to all sheets in the Excel file
+    format_excel.format(excel_filename)
